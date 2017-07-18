533ea5597a0f3bd42a0371d7754a29dd914eae0f
Amnisia <nxexox@gmail.com>
2017-07-18 18:54:42 +0500

    add middleware and migration

---
+++ multidomain/middleware.py
@@ -0,0 +1,36 @@
+import re
+
+from django.utils.deprecation import MiddlewareMixin
+
+from .models import domain_class
+
+
+class GetDomainMiddleware(MiddlewareMixin):
+    """
+    Add request.host with subdomain value or None
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Runs one time at the first request
+
+        """
+        super(GetDomainMiddleware, self).__init__(*args, **kwargs)
+        self.ipv4_re = re.compile(r"^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:?[0-9]{0,5}$")
+
+    def process_request(self, request):
+        request.host = None
+        domain, sub_domains = request.META['HTTP_HOST'], request.META['HTTP_HOST'].split('.')
+        count = 2
+
+        # TODO: Предусматриваем вариант обращения по IP а не по домену
+        if re.match(self.ipv4_re, domain):
+            count += 3
+
+        try:
+            request.domain = domain_class.objects.filter(
+                domain=".".join(sub_domains[:count - 1])
+            )
+        except domain_class.DoesNotExist:
+            request.domain = None
